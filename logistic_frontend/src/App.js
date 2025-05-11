8501c5f438bf78b9f7e05d9acc7a53f634bb4622
Ivan <ivan45sc@gmail.com>
2025-05-11 18:07:55 +0500

    add register page layout

--- logistic_frontend/src/App.js
+++ logistic_frontend/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
+import Register from './pages/auth/Register';
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ logistic_frontend/src/pages/auth/Register.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import './Register.css';
+
+const RegisterForm = () => {
+  return (
+    <div className='background'>
+    <div className="register-form">
+    <div class="underline-wrapper">
+        <h2>Регистрация</h2>
+    </div>
+      <form>
+        <div className="form-group email">
+            <p>Почта</p>
+          <input type="email" required />
+        </div>
+        <div className="form-group login">
+            <p>Логин</p>
+          <input type="text" required />
+        </div>
+        <div className="form-group phone">
+            <p>Номер телефона</p>
+          <input type="tel" required />
+        </div>
+        <div className="form-group password">
+            <p>Пароль</p>
+          <input type="password" required />
+        </div>
+        <div className="form-group password-confirm">
+            <p>Подтвердите пароль</p>
+          <input type="password" required />
+        </div>
+        <div className="checkbox-group">
+          <input type="checkbox" id="agreement" required />
+          <label htmlFor="agreement">
+            Я согласен на обработку персональных данных
+          </label>
+        </div>
+        <button type="submit">Зарегистрироваться</button>
+      </form>
+    </div>
+    </div>
+  );
+};
+
+export default RegisterForm;
